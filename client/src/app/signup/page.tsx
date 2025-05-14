d292d1abb965c34367a9fc15bbeb6d662761cfda
nomanoma121 <takeuchi180121@gmail.com>
2025-05-14 16:52:24 +0900

    feat: clientからapiを叩けるように実装

--- client/src/app/signup/page.tsx
+++ client/src/app/signup/page.tsx
@@ -10,121 +10,121 @@ import { useAuth } from "../../provider/auth";
 import { serverFetch } from "../../utils/fetch";
 
 type SignUpFormStateType = {
-	message: string;
+  message: string;
 };
 
 const SignUp = () => {
-	const { setUser } = useAuth();
-	const router = useRouter();
+  const { setUser } = useAuth();
+  const router = useRouter();
 
-	const SignUpFormAction = useCallback(
-		async (
-			_prevState: SignUpFormStateType,
-			formData: FormData,
-		): Promise<SignUpFormStateType> => {
-			const name = formData.get("name");
-			const email = formData.get("email");
-			const password = formData.get("password");
+  const SignUpFormAction = useCallback(
+    async (
+      _prevState: SignUpFormStateType,
+      formData: FormData
+    ): Promise<SignUpFormStateType> => {
+      const name = formData.get("name");
+      const email = formData.get("email");
+      const password = formData.get("password");
 
-			if (name === null || email === null || password === null) {
-				return { message: "メールアドレスとパスワードを入力してください" };
-			}
+      if (name === null || email === null || password === null) {
+        return { message: "メールアドレスとパスワードを入力してください" };
+      }
 
-			if (
-				typeof name !== "string" ||
-				typeof email !== "string" ||
-				typeof password !== "string"
-			) {
-				return { message: "不正な入力です" };
-			}
+      if (
+        typeof name !== "string" ||
+        typeof email !== "string" ||
+        typeof password !== "string"
+      ) {
+        return { message: "不正な入力です" };
+      }
 
-			if (password.length < 8) {
-				return { message: "パスワードは8文字以上で入力してください" };
-			}
+      if (password.length < 8) {
+        return { message: "パスワードは8文字以上で入力してください" };
+      }
 
-			const res = await serverFetch("/auth/signup", {
-				method: "POST",
-				body: JSON.stringify({ name, email, password }),
-				headers: {
-					"Content-Type": "application/json",
-				},
-			});
+      const res = await serverFetch("/auth/signup", {
+        method: "POST",
+        body: JSON.stringify({ user: { name, email, password } }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
 
-			if (res.ok) {
-				const data = await res.json();
-				localStorage.setItem("token", data.token);
-				setUser(data.user);
-				router.push("/home");
-				return { message: "" };
-			}
+      if (res.ok) {
+        const data = await res.json();
+        localStorage.setItem("token", data.token);
+        setUser(data.user);
+        router.push("/home");
+        return { message: "" };
+      }
 
-			return {
-				message: "登録に失敗しました",
-			};
-		},
-		[setUser, router],
-	);
+      return {
+        message: "登録に失敗しました",
+      };
+    },
+    [setUser, router]
+  );
 
-	const [error, submitAction, isPending] = useActionState(SignUpFormAction, {
-		message: "",
-	});
+  const [error, submitAction, isPending] = useActionState(SignUpFormAction, {
+    message: "",
+  });
 
-	return (
-		<form
-			action={submitAction}
-			className={css({
-				display: "flex",
-				flexDirection: "column",
-				alignItems: "center",
-				justifyContent: "center",
-				backgroundColor: "white",
-				borderRadius: "8px",
-				boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
-				marginTop: "100px",
-			})}
-		>
-			<h1>SignUp</h1>
-			<table className={css({ margin: "16px 0" })}>
-				<tbody>
-					<tr>
-						<td>名前</td>
-						<td>
-							<Input type="text" name="name" required />
-						</td>
-					</tr>
-					<tr>
-						<td>メールアドレス</td>
-						<td>
-							<Input type="email" name="email" required />
-						</td>
-					</tr>
-					<tr>
-						<td>パスワード</td>
-						<td>
-							<Input type="password" name="password" required />
-						</td>
-					</tr>
-				</tbody>
-			</table>
-			<Button type="submit" disabled={isPending}>
-				登録
-			</Button>
-			{error.message && (
-				<p
-					className={css({
-						color: "var(--danger-color)",
-						fontSize: "14px",
-					})}
-				>
-					{error.message}
-				</p>
-			)}
-			<p>
-				アカウントをお持ちの方は
-				<Link href="/login">こちら</Link>
-			</p>
-		</form>
-	);
+  return (
+    <form
+      action={submitAction}
+      className={css({
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "white",
+        borderRadius: "8px",
+        boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+        marginTop: "100px",
+      })}
+    >
+      <h1>SignUp</h1>
+      <table className={css({ margin: "16px 0" })}>
+        <tbody>
+          <tr>
+            <td>名前</td>
+            <td>
+              <Input type="text" name="name" required />
+            </td>
+          </tr>
+          <tr>
+            <td>メールアドレス</td>
+            <td>
+              <Input type="email" name="email" required />
+            </td>
+          </tr>
+          <tr>
+            <td>パスワード</td>
+            <td>
+              <Input type="password" name="password" required />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Button type="submit" disabled={isPending}>
+        登録
+      </Button>
+      {error.message && (
+        <p
+          className={css({
+            color: "var(--danger-color)",
+            fontSize: "14px",
+          })}
+        >
+          {error.message}
+        </p>
+      )}
+      <p>
+        アカウントをお持ちの方は
+        <Link href="/login">こちら</Link>
+      </p>
+    </form>
+  );
 };
 
 export default SignUp;
--- client/src/utils/fetch.ts
+++ client/src/utils/fetch.ts
@@ -1,7 +1,7 @@
 export const serverFetch = (url: string, options: RequestInit = {}) => {
 	const serverHost = "http://localhost:8000";
 	const token = localStorage.getItem("token");
-	return fetch(`${serverHost}${url}`, {
+	return fetch(`${serverHost}/api${url}`, {
 		...options,
 		headers: {
 			...(options.headers || {}),
