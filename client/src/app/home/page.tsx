e345529c143af33140434e2787d3ea64e18906ac
nomanoma121 <takeuchi.k.747@ms.saitama-u.ac.jp>
2025-05-14 10:10:36 +0900

    chore: format

--- client/src/app/home/page.tsx
+++ client/src/app/home/page.tsx
@@ -1,12 +1,15 @@
 const Home = () => {
-  return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the home page of our application.</p>
-      <p>Here you can find various resources and links to other sections of the app.</p>
-      <p>Feel free to explore!</p>
-    </div>
-  );
-}
+	return (
+		<div>
+			<h1>Welcome to the Home Page</h1>
+			<p>This is the home page of our application.</p>
+			<p>
+				Here you can find various resources and links to other sections of the
+				app.
+			</p>
+			<p>Feel free to explore!</p>
+		</div>
+	);
+};
 
 export default Home;
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,19 +1,22 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the home page of our application.</p>
-      <p>Here you can find various resources and links to other sections of the app.</p>
-      <p>Feel free to explore!</p>
-      <Image
-        src="/images/hero.png"
-        alt="Hero Image"
-        width={500}
-        height={300}
-        className="rounded-lg"
-      />
-    </div>
-  );
+	return (
+		<div>
+			<h1>Welcome to the Home Page</h1>
+			<p>This is the home page of our application.</p>
+			<p>
+				Here you can find various resources and links to other sections of the
+				app.
+			</p>
+			<p>Feel free to explore!</p>
+			<Image
+				src="/images/hero.png"
+				alt="Hero Image"
+				width={500}
+				height={300}
+				className="rounded-lg"
+			/>
+		</div>
+	);
 }
